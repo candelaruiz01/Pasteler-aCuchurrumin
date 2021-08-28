9af3bee86b041fa07ba93a84fe53f169e78d3d56
cami1452 <leon.camila.epet20@gmail.com>
2021-08-28 20:21:59 -0300

    footer

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,24 @@
 alert("Pulse aceptar para ingresar a la pagina de Pasteleria Cuchurrumin");
+
+document.querySelector('h1').addEventListener('click',()=>{
+    document.body.style.background='pink';
+})
+window.addEventListener("load",function(){
+	document.getElementById("play").addEventListener("click",sonarPajaros);
+	document.getElementById("stop").addEventListener("click",callarPajaros);			
+});
+
+function sonarSonido(){
+	var sonido = document.createElement("iframe");
+	sonido.setAttribute("src","audio/pajaritos.mp3");
+	document.body.appendChild(sonido);
+	document.getElementById("play").removeEventListener("click",sonarSonido);
+}
+
+function callarSonido(){
+	var iframe = document.getElementsByTagName("iframe");
+    if (iframe.length > 0){
+		iframe[0].parentNode.removeChild(iframe[0]);
+		document.getElementById("play").addEventListener("click",sonarSonido);
+	}
+}
